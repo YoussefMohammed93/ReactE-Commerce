f0acdda160946dfac5c2a13dd2b808145435a037
Youssef <ixyoussef46@gmail.com>
2024-05-26 00:37:07 +0300

    finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -112,7 +112,7 @@ export default function Header() {
         </NavLink>
         <button>
           <div className="profile">
-            <a href="/Contact">
+            <Link to="/Contact">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -127,7 +127,7 @@ export default function Header() {
                   d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
                 />
               </svg>
-            </a>
+            </Link>
           </div>
         </button>
         <button className="block md:hidden" onClick={() => setIsOpen(!isOpen)}>
